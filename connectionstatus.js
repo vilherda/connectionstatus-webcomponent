0379ee3055a891751dae1e481457bd496ddbb4fc
David Villace Hernandez <david.villace.sopra@axa.com>
2020-10-05 20:51:23 +0200

    patch: :bug: correction

--- connectionstatus.js
+++ connectionstatus.js
@@ -4,32 +4,35 @@ const OFFLINE_EVENT_NAME = 'offline';
 const INTERESTING_EVENTS_NAMES = [LOAD_EVENT_NAME, ONLINE_EVENT_NAME, OFFLINE_EVENT_NAME];
 const CONNECTION_STATUS_EVENT_NAME = 'connection-status-changed';
 
-function triggerEvent(status) {
-    const event = new CustomEvent(CONNECTION_STATUS_EVENT_NAME, { detail: status });
-    this.dispatchEvent(event);
+function triggerEvent(element, status) {
+    const event2launch = new CustomEvent(CONNECTION_STATUS_EVENT_NAME, { detail: status });
+    element.dispatchEvent(event2launch);
+    console.info(status);
 }
 
-function evalInterestingEvent(event) {
-    status = false;
-    if (event.type === LOAD_EVENT_NAME) {
-        status = navigator.onLine;
-    } else {
-        status = event.type === ONLINE_EVENT_NAME;
-    }
-    triggerEvent(status);
+function generateHandler(element) {
+    return function evalInterestingEvent(anEvent) {
+        status = anEvent.type === LOAD_EVENT_NAME ? navigator.onLine : anEvent.type === ONLINE_EVENT_NAME;
+        triggerEvent(element, status);
+    };
 }
 
 class ConnectionStatus extends HTMLElement {
+    constructor() {
+        super();
+        this.handler = generateHandler(this);
+        // window.customElements.define('connection-status', ConnectionStatus);
+    }
     connectedCallback() {
         INTERESTING_EVENTS_NAMES.forEach((eventName) => {
-            window.addEventListener(eventName, evalInterestingEvent);
+            window.addEventListener(eventName, this.handler);
         });
-        triggerEvent(navigator.onLine || false);
+        triggerEvent(this, navigator.onLine || false);
     }
 
     disconnectedCallback() {
         INTERESTING_EVENTS_NAMES.forEach((eventName) => {
-            window.removeEventListener(eventName, evalInterestingEvent);
+            window.removeEventListener(eventName, this.handler);
         });
     }
 }
