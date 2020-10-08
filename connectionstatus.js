cecbb094e5f96ae3cd3f6840903d45bc65a51d1c
David Villace Hernandez <david.villace.sopra@axa.com>
2020-10-08 16:23:28 +0200

    patch: ensure to send a boolean value

--- connectionstatus.js
+++ connectionstatus.js
@@ -5,13 +5,13 @@ const INTERESTING_EVENTS_NAMES = [LOAD_EVENT_NAME, ONLINE_EVENT_NAME, OFFLINE_EV
 const CONNECTION_STATUS_EVENT_NAME = 'connection-status-changed';
 
 function triggerEvent(element, status) {
-    const event2launch = new CustomEvent(CONNECTION_STATUS_EVENT_NAME, { bubbles: true, composed: true, detail: status });
+    const event2launch = new CustomEvent(CONNECTION_STATUS_EVENT_NAME, { bubbles: true, composed: true, detail: status == true });
     element.dispatchEvent(event2launch);
 }
 
 function generateHandler(element) {
     return function evalInterestingEvent(anEvent) {
-        status = anEvent.type === LOAD_EVENT_NAME ? navigator.onLine : anEvent.type === ONLINE_EVENT_NAME;
+        const status = anEvent.type == LOAD_EVENT_NAME ? navigator.onLine : anEvent.type == ONLINE_EVENT_NAME;
         triggerEvent(element, status);
     };
 }
@@ -25,6 +25,7 @@ class ConnectionStatus extends HTMLElement {
         INTERESTING_EVENTS_NAMES.forEach((eventName) => {
             window.addEventListener(eventName, this.handler);
         });
+        Boolean()
         triggerEvent(this, navigator.onLine || false);
     }
 
